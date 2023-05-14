4ea3e3756d579a864cf18ed2b5407b22186e4919
NimbleFerret <andrew@navy.online>
2023-05-14 19:57:04 +0300

    split project for venom and cronos separetly

---
+++ venom/scripts/1-deploy-collections.js
@@ -0,0 +1,31 @@
+async function main() {
+
+
+
+
+    const signer = await locklift.keystore.getSigner("0");
+    const nftArtifacts = await locklift.factory.getContractArtifacts("Nft");
+    const indexArtifacts = await locklift.factory.getContractArtifacts("Index");
+    const indexBasisArtifacts = await locklift.factory.getContractArtifacts("IndexBasis");
+    const { contract: sample, tx } = await locklift.factory.deployContract({
+        _mintPrice: "Collection",
+        publicKey: signer.publicKey,
+        initParams: {},
+        constructorParams: {
+            codeNft: nftArtifacts.code,
+            codeIndex: indexArtifacts.code,
+            codeIndexBasis: indexBasisArtifacts.code,
+            json: `{"collection":"tutorial"}` // EXAMPLE...not by TIP-4.2
+        },
+        value: locklift.utils.toNano(5),
+    });
+
+    console.log(`Collection deployed at: ${sample.address.toString()}`);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(e => {
+        console.log(e);
+        process.exit(1);
+    });
