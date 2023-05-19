48498886273f270b7e5dcd452eaefdc29e1e0518
NimbleFerret <andrew@navy.online>
2023-05-19 19:49:27 +0300

    clean code;

--- venom/locklift.config.ts
+++ venom/locklift.config.ts
@@ -23,7 +23,6 @@ const config: LockliftConfig = {
     // path: "/mnt/o/projects/broxus/TON-Solidity-Compiler/build/solc/solc",
 
     // Or specify version of compiler
-    // pragma ton-solidity ^0.58.1; 
     version: "0.62.0",
 
     // Specify config for extarnal contracts as in exapmple
@@ -41,7 +40,6 @@ const config: LockliftConfig = {
     // path: "/mnt/o/projects/broxus/TVM-linker/target/release/tvm_linker",
 
     // Or specify version of linker
-    // version: "0.61.2",
     version: "0.15.48",
   },
   networks: {
