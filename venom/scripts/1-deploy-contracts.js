b0bf936abf9b7ce646ad0c9c09150044527c1eb9
NimbleFerret <andrew@navy.online>
2023-05-22 14:25:48 +0300

    clean project

--- venom/scripts/1-deploy-contracts.js
+++ venom/scripts/1-deploy-contracts.js
@@ -91,26 +91,3 @@ main()
         console.log(e);
         process.exit(1);
     });
-
-
-// 2 - user 1
-const keys1 = {
-    publicKey: '29a4fc88f6c7cb3eef906cded87b111e777aac80d42db977a7dbcc9e7f2f8f59',
-    secretKey: '44f99156d0bb198f326ab92e185f6b9bc774aa4d4237f76ece6a417a00efc05b'
-};
-await locklift.keystore.addKeyPair('user1', {
-    publicKey: keys1.publicKey,
-    secretKey: keys1.secretKey
-});
-const user1Signer = await locklift.keystore.getSigner('user1');
-
-// 3 - user 2
-const keys2 = {
-    publicKey: '90fb122667100871be388d77250b305571e27b6d3c3896a1d0e02ff70554398c',
-    secretKey: '188fdf327227e3a90db81b7485c82dab264dd4ffbe569257a3a407342d929179'
-
-    //
-
-    phrase: 'flame viable ball first eager plate smooth absent news laugh mind drive',
-
-//
